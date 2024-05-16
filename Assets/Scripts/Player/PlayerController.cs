210a51f5b40498a235e26942642e49f5112784a1
ningblue <lemonpusi@gmail.com>
2024-05-16 23:53:28 +0800

    op- 修改了初始化场景 add- 增加了人物的移动和反转

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    public Vector2 inputDirection;
+    public float Speed;
+
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * Speed * Time.deltaTime, rb.velocity.y);
+        int faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+        {
+            faceDir = 1;
+        }
+        else if (inputDirection.x < 0)
+        {
+            faceDir = -1;
+        }
+        // 人物反转 localScale x轴的位置,如果位置和人物的比例发生变化,则对应数值也需要对应调整
+        transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+}
